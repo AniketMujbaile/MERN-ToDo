0a0460990bc1ba5ac0104aed5f0d3eb3b58637cf
Aniket Mujbaile <aniketmujbaile1@gmail.com>
2024-02-13 14:44:50 +0530

    first commit

---
+++ readme.md
@@ -0,0 +1,107 @@
+# Todo App
+
+This is a Todo App built with React.js for the frontend, Node.js and Express for the backend, and PostgreSQL for the database.
+
+### 🔗 Hosted link
+
+Hosted Link: [ToDo App]()
+
+## Features
+
+- User authentication with JWT token
+- CRUD operations for categories
+- CRUD operations for todos
+- CRUD operations for subtodos associated with each todo
+- Search functionality for todos
+- Pagination support for todos
+- Filtering todos by category
+
+## Technologies Used
+
+- React.js
+- TailwindCSS
+- Express.js
+- Postgres SQL
+- JSON Web Tokens (JWT)
+- bcrypt for password hashing
+
+## Project Structure
+
+The project is organized into two main folders: `client` and `server`.
+
+### Client
+
+The `client` folder contains the React.js application built with Tailwind for styling.
+
+#### Installation
+
+```bash
+cd client
+npm install
+```
+
+#### Usage
+
+```bash
+npm start
+```
+
+### Server
+
+The `server` folder contains the Node.js and Express backend, along with the PostgreSQL database.
+
+
+## Configuration File
+1. Create a PostgreSQL database and update the connection details in `server/.env`.
+
+```ENV
+
+PORT=5000
+
+DATABASE_URL=YOUR_URL
+
+SECRET=YOUR_KEY
+ 
+```
+#### Installation
+
+1. Install dependencies:
+
+```bash
+cd server
+npm install
+```
+
+2. Run the server:
+
+```bash
+npm start
+```
+
+## Endpoints
+
+- `POST /register`: Register a new user
+- `POST /login`: Login and obtain a JWT token
+- `GET /categories`: Get all categories
+- `POST /addCategory`: Add a new category
+- `DELETE /deleteCategory/:id`: Delete a category by ID
+- `GET /todos`: Get all todos with optional pagination, search, and category filter
+- `POST /addTodo`: Add a new todo
+- `DELETE /deleteTodo/:id`: Delete a todo by ID
+- `PUT /toggleTodo/:id`: Toggle the completion status of a todo
+- `GET /subtodos/:todoId`: Get all subtodos for a specific todo
+- `POST /addSubTodo`: Add a new subtodo to a specific todo
+- `DELETE /deleteSubTodo/:id`: Delete a subtodo by ID
+- `PUT /toggleSubTodo/:id`: Toggle the completion status of a subtodo
+
+## Screenshots
+<img src="" width="500px">
+<hr/>
+<img src="" width="500px">
+<hr/>
+<img src="" width="500px">
+<hr/>
+
+ 
+
+
---
+++ server/configs/db.js
@@ -0,0 +1,88 @@
+const { Pool } = require('pg');
+
+
+const pool = new Pool({
+  connectionString:process.env.DATABASE_URL, 
+  ssl: {
+    require: true,
+  },
+});
+
+
+
+const createUserTableQuery = `
+  CREATE TABLE IF NOT EXISTS users (
+    uid SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE,
+    pass VARCHAR(255)
+  );
+`;
+
+const createTodosTableQuery = `
+  CREATE TABLE IF NOT EXISTS todos (
+    id SERIAL PRIMARY KEY,
+    user_id INT,
+    title VARCHAR(255) NOT NULL,
+    completed BOOLEAN NOT NULL DEFAULT false,
+    category_id INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(uid) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE ON UPDATE CASCADE
+  );
+`;
+
+const createcategoriesTableQuery = `
+CREATE TABLE IF NOT EXISTS categories (
+  id SERIAL PRIMARY KEY,
+  category_name VARCHAR(255) NOT NULL,
+  display_name VARCHAR(255) NOT NULL
+);
+`
+
+const createSubTodosQuery = `
+CREATE TABLE IF NOT EXISTS subTodos (
+  id SERIAL PRIMARY KEY,
+  user_id INT,
+  todo_id INT,
+  title VARCHAR(255) NOT NULL,
+  completed BOOLEAN NOT NULL DEFAULT false,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES users(uid) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (todo_id) REFERENCES todos(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+`;
+
+const DropTablesQuery = `
+DROP TABLE IF EXISTS subTodos;
+DROP TABLE IF EXISTS todos;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS catagories;
+`
+
+// Execute table creation queries
+async function createTables() {
+  try {
+    const client = await pool.connect();
+    await client.query(createUserTableQuery);
+    await client.query(createcategoriesTableQuery);
+    await client.query(createTodosTableQuery);
+    await client.query(createSubTodosQuery);
+    // await client.query(DropTablesQuery)
+    console.log('Tables created successfully');
+    client.release();
+  } catch (err) {
+    console.error('Error creating tables:', err);
+  } 
+}
+
+// Call the function to create tables
+createTables();
+
+module.exports = pool
+
+
+
+
